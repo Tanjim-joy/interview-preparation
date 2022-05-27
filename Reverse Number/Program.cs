56e01b1611f804c4431266c8902b4e009c8478a2
Md. Tangimul haque <72943561+Tanjim-joy@users.noreply.github.com>
2022-05-27 15:38:06 +0600

    Reverse of a Number
    Reverse of a Number

---
+++ Reverse Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Reverse the Number!");           
+            Console.WriteLine("Please note that zeros will be ignored while reversing the number.");
+            
+            Console.Write("Enter Your Number: ");
+
+            int num = int.Parse(Console.ReadLine());
+            int result = 0;
+            while (num != 0)
+            {
+                int rem = num % 10;
+                result = result * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("The reversed number is: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
